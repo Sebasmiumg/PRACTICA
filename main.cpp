88341f4858bd569fac03ed56169d7cac8c6ea027
Sebasmiumg <jrossac@miumg.edu.gt>
2024-02-27 16:57:48 -0600

    jajaja

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char letra;
 
+    cout<<"Ingrese una letra minuscula: ";
+    cin>>letra;
+
+    if (letra == 'a' || letra=='e' || letra=='i' || letra=='o' || letra=='u' ){
+        cout<<"la letra ingresada es una vocal"<<endl;
+    }else{
+        cout<<"la letra que ingreso es una consonante"<<endl;
+    }
+
+
+    return 0;
 }
